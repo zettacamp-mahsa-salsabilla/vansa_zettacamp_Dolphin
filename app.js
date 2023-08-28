8cf76275ae02410ab30ef795b2669e7fd91131c9
zettacamp-mahsa-salsabilla <141140414+zettacamp-mahsa-salsabilla@users.noreply.github.com>
2023-08-28 22:47:24 +0700

    Javascript_Day1

---
+++ app.js
@@ -0,0 +1,39 @@
+//number 1 
+let favBook = 'John Davison Rockefeller'
+const favBook2 = 'Henry Ford'
+  console.log(favBook)
+  console.log(favBook2)
+  
+//number 1, point a
+favBook= 'Novel'
+console.log(favBook)
+
+//number 1, point b
+//favBook2 = 'Walt Disney'
+//console.log(favBook2)
+
+//fix
+delete favBook2;
+console.log(favBook2);
+
+//number 2
+let newBook = favBook +', '+favBook2
+console.log(newBook)
+
+
+//Try declare more variable using other data type with dynamic value and primitive value
+const author = 'Mahsa Vania Salsabilla'
+  console.log(author)
+
+let age = 18 
+  console.log(typeof age)
+age = 22
+  console.log(age)
+  
+const book = ['Bill Gates','Henry Ford','Enzo Ferrari','Walt Disney',['part1','part2','part3']];
+console.log(book);
+console.log(book[2]);
+console.log(typeof book);
+console.log(book.length);
+console.log(book[4][2]);
+  
